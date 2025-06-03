95d4d7113fe06ada5b1d2bb58f2ca003a285e59d
Santos2175 <santoshgurung949@gmail.com>
2025-06-03 07:45:34 +0545

    update: step to run test added

---
+++ src/tests/api.test.ts
@@ -0,0 +1,16 @@
+import { describe, test, expect } from "vitest";
+
+const person = {
+  isActive: true,
+  age: 32,
+};
+
+describe("person", () => {
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+});
